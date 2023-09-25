10f70fe0532e74e44ca4f315534c70325e36dd01
Alex William Derry <awfderry@gmail.com>
2023-09-25 13:53:14 -0700

    script to generate all quant results

--- src/protein-eval.py
+++ src/protein-eval.py
@@ -1,78 +1,66 @@
-from model_selection import top_model_confusion, top_model_preds, top_model_continuous_avg, top_model_continuous_iid
-from utils import serialize, deserialize, serialize_model, deserialize_model
+from evaluation import test_eval, extract_params, get_test_metrics
+import utils
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", "is_categorical_dtype")
+warnings.filterwarnings("ignore", "use_inf_as_na")
 
 encoders = ['COLLAPSE', 'ESM', 'AA']
-metal = 'CA'
+metal = 'ZN'
 
 conf_metrics = ["msd", "specificity", "precision", "fnr", "fdr", "recall", "accuracy", "balanced_acc", "correlation", "threat_score", "prevalence", "dice", "jaccard"]
 cont_metrics = ["auroc", "auprc", "ap"]
+test_metrics = conf_metrics + cont_metrics
 
-confusion_all = []
-confusion_class1 = []
-datum_level = []
-continuous_avg = []
-continuous_iid = []
+encoder_top_models = \
+    {'COLLAPSE': ('k20_r0_cutoff4.00_alpha0.001_tau0.00_lamnan.model', 0.9), \
+    'ESM': ('k30_r1_cutoff4.00_alpha0.500_tau1.00_lamnan.model', 0.0), \
+    'AA': ('k21_r1_cutoff8.00_alpha0.100_tau1.00_lamnan.model', 0.5)}
 
-confusion_all_models = {}
-confusion_class1_models = {}
-datum_level_models = {}
-continuous_avg_models = {}
-continuous_iid_models = {}
+baseline_top_models = \
+    {'COLLAPSE': ('COLLAPSE-ZN-8.0-0.0005', 0.7), \
+    'ESM': ('ESM-ZN-8.0-0.0005', 0.4), \
+    'AA': ('AA-ZN-8.0-0.0005', 0.6)}
 
-for encoder in encoders:
-    print('on encoder', encoder)
+base_df = []
+for encoder in ['COLLAPSE', 'ESM', 'AA']:
+    best_model, best_thresh = baseline_top_models[encoder]
+    results_dict = utils.deserialize(f'../data/baselines/{encoder}_test_results.pkl')
+    df = get_test_metrics(results_dict, encoder, best_model, best_thresh, test_metrics)
+    base_df.append(df)
+base_df = pd.concat(base_df)
+
+test_df = []
+for encoder, (model_str, threshold) in encoder_top_models.items():
     results_cache_dir = f"../data/{encoder}_{metal}_gridsearch_results/{encoder}-eval_results"
     model_cache_dir = f"../data/{encoder}_{metal}_gridsearch_results/{encoder}-fitted_k2_models"
+    processor_cache_dir = f"../data/{encoder}_{metal}_gridsearch_results/{encoder}-fitted_k2_processors"
     linearized_cache_dir = f"../data/{encoder}_{metal}_gridsearch_results/{encoder}-linearized_data"
+
+    _,_,cutoff,_,_,_ = extract_params(model_str)
+
+    if encoder == 'AA':
+        g_encoder = 'COLLAPSE'
+    else:
+        g_encoder = encoder
+
+    G_dir = f"../data/{g_encoder}_{metal}_{cutoff}_test_graphs"
     
-    print('Confusion metrics (all)')
-    row = []
-    for metric_str in conf_metrics:
-        best_model, best_thresh, best_score, stability = top_model_confusion(metric_str,results_cache_dir, model_cache_dir)
-        row.append(best_score)
-        confusion_all_models[metric_str] = (best_model, best_thresh)
-    confusion_all.append(row)
+    df = test_eval(model_str, threshold, test_metrics, model_cache_dir, processor_cache_dir, G_dir, gt_dir=None, label_dict=None, modality="graph")
+    test_df.append(df)
+test_df = pd.concat(test_df)
 
-    print('Confusion metrics (class-1)')
-    row = []
-    for metric_str in conf_metrics:
-        best_model, best_thresh, best_score, stability = top_model_confusion(metric_str,results_cache_dir, model_cache_dir, eval_class=1)
-        row.append(best_score)
-        confusion_class1_models[metric_str] = (best_model, best_thresh)
-    confusion_class1.append(row)
+combined_df = pd.concat([test_df, base_df])
+combined_df['method'] = ['K2']*len(test_df) + ['GAT+GNNExplainer']*len(base_df)
 
-    print('Datum-level metrics')
-    row = []
-    for metric_str in cont_metrics:
-        best_model, best_score = top_model_preds(metric_str, results_cache_dir, model_cache_dir)
-        row.append(best_score)
-    datum_level.append(row)
+mean_df = combined_df.groupby(['encoder', 'method','regime', 'metric'])['value'].mean().reset_index()
+sem_df = combined_df.groupby(['encoder', 'method','regime', 'metric'])['value'].sem().reset_index()
 
-    print('Continuous metrics (avg)')
-    row = []
-    for metric_str in cont_metrics:
-        best_model, best_score = top_model_continuous_avg(metric_str, results_cache_dir, model_cache_dir)
-        row.append(best_score)
-        continuous_avg_models[metric_str] = best_model
-    continuous_avg.append(row)
+mean_pvt = mean_df.pivot(index=['encoder', 'method', 'regime'], columns='metric', values='value')
+mean_pvt = mean_pvt[test_metrics]
 
-    print('Continuous metrics (iid)')
-    row = []
-    for metric_str in cont_metrics:
-        best_model, best_score = top_model_continuous_iid(metric_str, model_cache_dir, linearized_cache_dir)
-        row.append(best_score)
-        continuous_iid_models[metric_str] = best_model
-    continuous_iid.append(row)
-    
-    serialize(confusion_all_models, f"../data/results/{encoder}_{metal}_confusion_all_train_models.pkl")
-    serialize(confusion_class1_models, f"../data/results/{encoder}_{metal}_confusion_class1_train_models.pkl")
-    serialize(datum_level_models, f"../data/results/{encoder}_{metal}_datum_level_train_models.pkl")
-    serialize(continuous_avg_models, f"../data/results/{encoder}_{metal}_continuous_avg_train_models.pkl")
-    serialize(continuous_iid_models, f"../data/results/{encoder}_{metal}_continuous_iid_train_models.pkl")
+sem_pvt = sem_df.pivot(index=['encoder', 'method', 'regime'], columns='metric', values='value')
+sem_pvt = sem_pvt[test_metrics]
 
-pd.DataFrame(confusion_all, index=encoders, columns=conf_metrics).to_csv(f'../data/results/{metal}_confusion_all_train_scores.csv')
-pd.DataFrame(confusion_class1, index=encoders, columns=conf_metrics).to_csv(f'../data/results/{metal}_confusion_class1_train_scores.csv')
-pd.DataFrame(datum_level, index=encoders, columns=cont_metrics).to_csv(f'../data/results/{metal}_datum_level_train_scores.csv')
-pd.DataFrame(continuous_avg, index=encoders, columns=cont_metrics).to_csv(f'../data/results/{metal}_continuous_avg_train_scores.csv')
-pd.DataFrame(continuous_iid, index=encoders, columns=cont_metrics).to_csv(f'../data/results/{metal}_continuous_iid_train_scores.csv')
+mean_pvt.to_csv(f'../data/results/all_test_results_mean.csv')
+sem_pvt.to_csv(f'../data/results/all_test_results_sem.csv')
